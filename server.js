5744517be7b3b7d45b09597a289c804443ed375a
Matthijs Boet <m.boet@chello.nl>
2021-01-19 18:45:06 +0100

    created routes, starting implementation routes of users, contacts and auth

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({msg: "Home route"})
+})
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Contact Keeper Server started at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`API Server started at http://localhost:${PORT}`));
