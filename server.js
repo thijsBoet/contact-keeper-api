b24dfcab8c25bb7313572e547356b22e907a9458
Matthijs Boet <m.boet@chello.nl>
2021-01-21 02:20:26 +0100

    finished API CRUD and Auth implemented for User and Contacts

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({msg: "Home route"})
-})
+connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Home route" });
+});
+
+// Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`API Server started at http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`API Server started at http://localhost:${PORT}`)
+);
